bdcd5e519b806b601faa240bce545d98420f26b8
TCSSLaTrobe <TCSSLaTrobe@gmail.com>
2024-08-25 10:49:00 +1000

    Update Admin_Login.py

--- Admin_Login.py
+++ Admin_Login.py
@@ -1,11 +1,22 @@
-from flask import redirect,request,render_template,url_for
+from flask import Flask, redirect,request,url_for,flash
+import sqlite3
+import string
+import random
+app=Flask(__name__)
+app.secret_key='your_secret_key'
+DATABASE='TCSS.db'
 
-def login_check():
-    u_name=request.form["username"]
-    pass1=request.form["password"]
-    if(u_name='Neha@1' and pass1=='password@123'):
-        return redirect("Welcome.html")
-    elif(u_name==pass1):
-        return redirect('login.html')
-    else:
-        return redirect('login.html')
+def get_db_connection():
+    conn=sqlite3.connect(DATABASE)#connect
+    conn.row_factory=sqlite3.Row
+    return conn
+def create_table():
+    conn=get_db_connection()
+    cursor=conn.cursor()
+    cursor.execute(" CREATE TABLE IF NOT EXISTS user(loginid varchar (10), "
+                   "PRIMARY KEY, pass_word varchar(18) NOT NULL, user_type varchar(15)")
+
+    cursor.execute(" CREATE TABLE IF NOT EXISTS lecturer(lect_name varchar (10), "
+                   "Subject_name (255), workload integer) ")
+    conn.commit()
+    conn.close()
