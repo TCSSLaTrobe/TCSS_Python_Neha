233c525fcd3112125b24ec70aed0cb023b61de23
TCSSLaTrobe <TCSSLaTrobe@gmail.com>
2024-09-04 21:48:00 +1000

    Update Admin_Login.py

--- Admin_Login.py
+++ Admin_Login.py
@@ -49,10 +49,48 @@ def admin_login():
             return redirect(url_for('admin_homepage'))
         else:
             flash("Invalid Credentials. Please Retry Logging In")
-            return render_template('admin_login.html')
     return render_template('admin_login.html')
+
+@app.route('/addlecturer_subject', methods=['GET', 'POST'])
+def add_lecturer():
+    if request.method == 'POST':
+        lect_name = request.form['LecturerName']
+        sub_name = request.form['subjectPrefix']
+        workload = request.form['lecturerLoad']
+        login_id = 'Lect' + lect_name[:4]
+        password1 = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(8))
+        usertype = 'lecturer'
+        
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        
+        cursor.execute("""
+            INSERT INTO lecturer (lect_name, subject_name, workload)
+            VALUES (?, ?, ?)
+        """, (lect_name, sub_name, workload))
+        
+        cursor.execute("""
+            INSERT INTO user (loginid, pass_word, user_type)
+            VALUES (?, ?, ?)
+        """, (login_id, password1, usertype))
+        
+        conn.commit()
+        conn.close()
+        
+        flash('Lecturer Details Added Successfully')
+        return redirect(url_for('admin_homepage'))
+    
+    return render_template('addlecturer_subject.html')
+
+@app.route('/view_logindetails', methods=['GET'])
+def view_logindetails():
+    conn = get_db_connection()
+    users = conn.execute('SELECT loginid, user_type, pass_word FROM user').fetchall()
+    conn.close()
+    return render_template('view_logindetails.html', users=users)
 
 if __name__ == '__main__':
     create_table()
     app.run(debug=True)
+
 
